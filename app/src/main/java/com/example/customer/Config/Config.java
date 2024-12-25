e919a071982b59c26a203a845d91482f2e441726
thanhvinh-htnbt <147599687+thanhvinh-htnbt@users.noreply.github.com>
2024-12-25 19:21:43 +0700

    ignore

--- app/src/main/java/com/example/customer/Config/Config.java
+++ app/src/main/java/com/example/customer/Config/Config.java
@@ -1,7 +1,7 @@
 package com.example.customer.Config;
 
 public class Config {
-    public static final String ip="192.168.1.6";
+    public static final String ip="192.168.1.5";
     public static final int port=9090;
     public static final String userInfo="userInfo"; // file lưu thông tin của user
 }
